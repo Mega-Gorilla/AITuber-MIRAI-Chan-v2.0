df1eaae2d05857cb8c262d5b2a859f1ea935f92e
Mega Gorilla <hahahadesu@gmail.com>
2023-09-11 19:00:19 +0900

    Queue方式実装。

--- azure_speech_handler.py
+++ azure_speech_handler.py
@@ -1,33 +1,33 @@
-# speech_handler.py
+# azuer_speech_handler.py
 import azure.cognitiveservices.speech as speechsdk
 import os
 import asyncio
+import time
+from rich import print
 
 class SpeechHandler:
-    def __init__(self, speech_key, speech_region, language='ja-JP'):
-        self.done = asyncio.Event()  # Use asyncio.Event to manage the state
+    def __init__(self, queue, speech_key, speech_region, language='ja-JP',debug=True):
+        self.done = False
+        self.loop = asyncio.get_event_loop()
         self.speech_key = speech_key
         self.speech_region = speech_region
         self.EndSilenceTimeoutMs = "1000"
         self.language = language
         self.speech_recognizer = None
-        self.recognized_text = ""
-        self.speech_state = None
-
-    def end_speech(self):
-        self.done.set()
-
+        self.result_text = ""
+        self.queue = queue
+        self.debug = debug
+        
     def _recognized_handler(self, evt):
-        self.recognized_text = evt.result.text
-        print(f'RECOGNIZED: {self.recognized_text}')
+        self.result_text = evt.result.text
+        if self.result_text !="":
+            self.done=True
+        if self.debug:
+            print(f'RECOGNIZED: {evt.result.text}')
     
-    def _speech_start_detected_handler(self, evt):
-        self.speech_state = True
-        print(f'SPEECH STARTED')
-
-    def _speech_end_detected_handler(self, evt):
-        self.speech_state = False
-        print(f'SPEECH ENDED')
+    def session_started(self, evt):
+        self.result_text = ""
+        self.done = False
 
     async def from_mic(self):
         speech_config = speechsdk.SpeechConfig(subscription=self.speech_key, region=self.speech_region, speech_recognition_language=self.language)
@@ -35,30 +35,40 @@ class SpeechHandler:
         audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
         self.speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
 
-        self.speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
         self.speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
+        self.speech_recognizer.canceled.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
         self.speech_recognizer.recognized.connect(self._recognized_handler)
-        self.speech_recognizer.speech_start_detected.connect(self._speech_start_detected_handler)
-        self.speech_recognizer.speech_end_detected.connect(self._speech_end_detected_handler)
+        self.speech_recognizer.session_started.connect(self.session_started)
+        
+        if self.debug:
+            print("[green]Speak into your microphone.[/green]")
 
         self.speech_recognizer.start_continuous_recognition()
-
-        print("Speak into your microphone.")
+        while not self.done:
+            time.sleep(0.1)
         
-        await self.done.wait()  # Wait until self.done is set
-
+        if self.debug:
+            print("[red]Azure Speech Stopped..[/red]")
+            
         self.speech_recognizer.stop_continuous_recognition()
+        await self.queue.put(self.result_text)
 
-        return self.recognized_text, self.speech_state
-    
+async def handle_results(queue):
+    while True:
+        result = await queue.get()
+        print(f"Received result: {result}")
+        queue.task_done()
+
 async def main():
-    handler = SpeechHandler(speech_key, speech_region)
-    result_text, speech_state = await handler.from_mic()  # Wait for completion asynchronously
+    queue = asyncio.Queue()
+    handler = SpeechHandler(queue,speech_key, speech_region)
 
-    print(f"Last recognized text: {result_text}, Final speech state: {speech_state}")
+    # Start a task to handle results
+    asyncio.create_task(handle_results(queue))
 
-    # If you want to stop it at some point
-    # handler.end_speech()
+    while True:
+        asyncio.create_task(handler.from_mic())
+        await asyncio.sleep(1)
 
 if __name__ == "__main__":
     speech_key = os.getenv("AZURE_API_KEY")
--- main.py
+++ main.py
@@ -1,32 +1,34 @@
-import azure.cognitiveservices.speech as speechsdk
+#main.py
+from azure_speech_handler import SpeechHandler
 from rich import print
 import os
 import openai
+import asyncio
 
 #keys
 azure_key = os.getenv("AZURE_API_KEY")
 speech_region = "japaneast"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-#Azure_speech
-def Azure_speech(speech_key,speech_region):
-    #setting.
-    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=speech_region, speech_recognition_language='ja-JP')
-    audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
-    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+async def handle_results(queue):
+    while True:
+        result = await queue.get()
+        print(f"Received result: {result}")
+        queue.task_done()
 
-    speech_recognizer.recognized.connect(on_recognized)
-    speech_recognizer.canceled.connect(on_canceled)
-    speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
-    speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
-    speech_recognizer.start_continuous_recognition()
+async def main():
+    queue = asyncio.Queue()
+    handler = SpeechHandler(queue,speech_key, speech_region)
 
-def on_recognized(args):
-    recognized_text = args.result.text
-    print(recognized_text)
-def on_canceled(args):
-    if args.reason == speechsdk.CancellationReason.Error:
-        print(f"音声認識エラーが発生しました: {args.error_details}")
+    # Start a task to handle results
+    asyncio.create_task(handle_results(queue))
+
+    while True:
+        asyncio.create_task(handler.from_mic())
+        await asyncio.sleep(1)
 
 if __name__ == "__main__":
-    Azure_speech(azure_key,speech_region)
+    speech_key = os.getenv("AZURE_API_KEY")
+    speech_region = "japaneast"
+
+    asyncio.run(main())
