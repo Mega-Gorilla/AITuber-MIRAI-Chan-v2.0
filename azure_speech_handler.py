8b6e4c74a8b4bbc7791e39ee17af8ecb93fd3253
Mega Gorilla <hahahadesu@gmail.com>
2023-09-13 01:03:14 +0900

    細かい変更が出来るようにアップデート

--- azure_speech_handler.py
+++ azure_speech_handler.py
@@ -6,17 +6,19 @@ import time
 from rich import print
 
 class SpeechHandler:
-    def __init__(self, queue, speech_key, speech_region, language='ja-JP',debug=True):
+    def __init__(self, queue, producer_id,speech_key, speech_region, language='ja-JP',mic_id=None,TimeoutMs='3000',debug=True):
         self.done = False
         self.loop = asyncio.get_event_loop()
         self.speech_key = speech_key
         self.speech_region = speech_region
-        self.EndSilenceTimeoutMs = "1000"
+        self.device_ID = mic_id
+        self.EndSilenceTimeoutMs = TimeoutMs
         self.language = language
         self.speech_recognizer = None
         self.result_text = ""
         self.queue = queue
         self.debug = debug
+        self.producer_id = producer_id
         
     def _recognized_handler(self, evt):
         self.result_text = evt.result.text
@@ -32,11 +34,19 @@ class SpeechHandler:
     async def from_mic(self):
         speech_config = speechsdk.SpeechConfig(subscription=self.speech_key, region=self.speech_region, speech_recognition_language=self.language)
         speech_config.set_property(speechsdk.PropertyId.SpeechServiceConnection_EndSilenceTimeoutMs, self.EndSilenceTimeoutMs)
-        audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
+        if self.device_ID != None:
+            audio_config = speechsdk.audio.AudioConfig(device_name=str(self.device_ID))
+            if self.debug:
+                print(f"setup mic id {self.device_ID}")
+        else:
+            if self.debug:
+                print(f"defaltmic {self.device_ID}")
+            audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
+        
         self.speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
 
-        self.speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
-        self.speech_recognizer.canceled.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
+        #self.speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
+        #self.speech_recognizer.canceled.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
         self.speech_recognizer.recognized.connect(self._recognized_handler)
         self.speech_recognizer.session_started.connect(self.session_started)
         
@@ -51,7 +61,7 @@ class SpeechHandler:
             print("[red]Azure Speech Stopped..[/red]")
             
         self.speech_recognizer.stop_continuous_recognition()
-        await self.queue.put(self.result_text)
+        await self.queue.put({"ID":self.producer_id,"message":self.result_text})
 
 async def handle_results(queue):
     while True:
