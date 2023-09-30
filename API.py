1ef1eba3d8fcfb4b8392fc4788f46e82aff908c6
Mega Gorilla <hahahadesu@gmail.com>
2023-10-01 00:28:34 +0900

    FastAPIを消去

--- API.py
+++ API.py
@@ -1,10 +1,8 @@
 #API.py
 from module.azure_speech_handler import SpeechHandler
 from rich import print
-from fastapi import FastAPI
 import os
 import asyncio
-import time
 import httpx
 
 #keys
@@ -12,30 +10,38 @@ speech_key = os.getenv("AZURE_API_KEY")
 speech_region = "japaneast"
 
 #URL
-openai_post = "http://127.0.0.1:8000/requst/openai-post"
-openai_get = "http://127.0.0.1:8000/requst/openai-get/queue"
+BASE_URL = "http://127.0.0.1:8000"
 
-#global
-UserName = "猩々博士"
-talkFlag = False
-speechToText = True
+class GlobalValues:
+    UserName = "猩々博士"
+    chara_profile="""みらい-女子高生ギャルAITuber。AIだが配信者をしている。
+    博士-みらいの開発者。正体はゴリラ。"""
+    talkFlag = False
+    speechToText = True
 
-#FastAPI
-app = FastAPI()
-@app.post("/request/genereteTalk",tags=["Request"])
-async def TalkJob_request():
-    global talkFlag
-    talkFlag = True
-    return{"/request/genereteTalk":talkFlag}
+#
+async def post_data(data_dict):
+    async with httpx.AsyncClient() as client:
+        response = await client.post(f"{BASE_URL}/custom/add", json=data_dict)
+        if response.status_code == 200:
+            print("Data posted successfully!")
+            return response.json()
+        else:
+            print(f"Failed to post data. Status code: {response.status_code}")
+            return None
 
-@app.post("/request/speechToText",tags=["Request"])
-async def speechToText_request():
-    global speechToText
-    speechToText = not speechToText
-    return{"/request/speechToText":speechToText}
+async def get_data():
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"{BASE_URL}/custom/get_data/")
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Failed to get data. Status code: {response.status_code}")
+            return None
 
 #GPT Manager
-async def request_GPT(prompt_name, user_prompt, variables,stream):
+#GPTにリクエストする
+async def request_GPT(ID,prompt_name, user_prompt, variables,stream):
     data = {
         "user_assistant_prompt": user_prompt,
         "variables": variables
@@ -44,12 +50,12 @@ async def request_GPT(prompt_name, user_prompt, variables,stream):
     params = {
         "stream": stream  # または True
     }
-    request_URL = (f"{openai_post}/{prompt_name}")
+    request_URL = (f"{BASE_URL}/requst/openai-post/{prompt_name}")
     
     print(data)
     async with httpx.AsyncClient() as client:
         response = await client.post(request_URL, json=data, params=params)
-        return {"ID":'talkStr',"message":response}
+        return {"ID":ID,"message":response}
     
 async def get_queue_request(url):
     try:
@@ -69,59 +75,74 @@ async def get_queue_request(url):
         print(f"An error occurred: {err}")
         return None
 
-#新しいasyncioタスクを作成するプロセス
-async def create_tasks(task_queue):
-    #新しいタスクを作成
-    if task_queue.qsize()!=0:
-        tasks= await task_queue.get()
-        if asyncio.iscoroutine(tasks[1]):
-            newjob = asyncio.create_task(tasks[1])
-            job_manager.add_job(tasks[0],newjob)
-        else:
-            for task in tasks:
-                newjob = asyncio.create_task(task[1])
-                job_manager.add_job(task[0],newjob)
-
-# result_queueを監視し、新しい関数が入力されたら処理するプロセス
-#handle_resultsは無限ループする。
+# 処理結果に基づいて次のタスクを決定する
 async def handle_results(result_queue,task_queue):
     chatLog=''
+    chatLog_lineMax=5
     summary=''
     mirai_example=''
 
     while True:
+        global talkFlag
+
+        #結果を取得する
         result = await result_queue.get()
-        #print(f"Received result: {result}")
-        print(result["message"],end=None)
+        result_ID = result["ID"]
+        result_message = result["message"]
+        print(f"結果ID:{result_ID}/nMessage:{result_message}\n\n")
         
-        if result["ID"] == 'speech':
+        # User会話データがあった時、
+        if result_ID == 'speech':
             #User 会話履歴の追加
-            userTalkStr=result['message']
-            chatLog += (f"{UserName}:{userTalkStr}\n")
+            userTalkStr = result_message
+            chatLog += (f"{GlobalValues.UserName}:{userTalkStr}\n")
 
             #メモリより類似会話の検索
 
             if talkFlag: #Talkボタンが押された時
-                global talkFlag
                 talkFlag = False
 
-                miraiChanTalkStr= await request_GPT(prompt_name='miraiV2.json',
-                                  user_prompt=[],
-                                      variables={
-                                          "chatLog":chatLog,
-                                          "summary":summary,
-                                          "mirai_example":mirai_example},
-                                          stream=True)
-                #みらい　会話履歴の追加
-                chatLog+=(f"Mirai:{miraiChanTalkStr}\n")
-                result_queue.put(miraiChanTalkStr)
-        
+                miraichan_talk_task = request_GPT(ID="talkStr",
+                                                  prompt_name='miraiV2.json',
+                                                  user_prompt=[],
+                                                  variables={
+                                                      "chatLog":chatLog,
+                                                        "summary":summary,
+                                                        "mirai_example":mirai_example},
+                                                    stream=True)
+                await task_queue.put(miraichan_talk_task)
+
+        # Mirai Chan Talk が出力された時、
+        if result_ID == 'talkStr':
+            #みらい　会話履歴の追加
+            chatLog+=(f"Mirai:{result_message}\n")
+
+        # 要約が出力されたとき、
+        if result_ID == 'summaryStr':
+            #要約変数に要約結果を追加
+            summary = result_message
+            #ショートメモリから要約した内容を消去
+            chatlog_lines = chatLog.split("\n")
+            chatLog = "\n".join(chatlog_lines[:-chatLog_lineMax])
+
+        # ショートメモリ(chatlog)が一定行数以上になった時要約を依頼
+        if chatLog.count('\n')+1 > chatLog_lineMax:
+            #ショートメモリが一定数以上になった時要約する
+            summary_task = request_GPT(ID='summaryStr',
+                                       prompt_name='talkSummary',
+                                       user_prompt=[],
+                                       variables={
+                                                "charactor_profile":GlobalValues.chara_profile, 
+                                                "summary":summary,
+                                                "chat_log":chatLog},
+                                        stream=True)
+            await task_queue.put(summary_task)
         
         await asyncio.sleep(0.1)
 
 async def main():
-    task_queue = asyncio.Queue(maxsize=10)
-    result_queue = asyncio.Queue(maxsize=100)
+    task_queue = asyncio.Queue()
+    result_queue = asyncio.Queue()
 
     #azure setup
     handler = SpeechHandler(queue=result_queue,
@@ -134,14 +155,38 @@ async def main():
 
     # Start a task to handle results
     asyncio.create_task(handle_results(result_queue,task_queue))
+    
+    async def wrapper(func, *args):
+        result = await func(*args)
+        await result_queue.put(result)
 
     while True:
-        
+        global_dict = await get_data()
+
+        if "talkFlag" in global_dict:
+            GlobalValues.talkFlag = global_dict["talkFlag"]
+        if "speechToText" in global_dict:
+            GlobalValues.speechToText = global_dict["speechToText"]
+
         #Azure Speechが終了or実行されていない時、Speechを起動する。
-        if  speechToText and (speech_task is None or speech_task.done()):
+        print(GlobalValues.speechToText)
+        if  GlobalValues.speechToText and (speech_task is None or speech_task.done()):
+            print("OK")
             speech_task=asyncio.create_task(handler.from_mic())
         
-        await create_tasks(task_queue)
+        #新しいタスクを作成
+        tasks_to_run = []
+
+        if task_queue.qsize()!=0:
+            tasks= await task_queue.get()
+        #タスクリストから、子タスクを分割し、タスクリストに変換する。
+        for task in tasks:
+            add_task=(task['func'],task['value'])
+            tasks_to_run.append(add_task)
+        
+        # 定義に基づいてタスクを動的に生成
+        tasks = [asyncio.create_task(wrapper(func, *args)) for func, args in tasks_to_run]
+        await asyncio.gather(*tasks)
             
         await asyncio.sleep(0.5)
 
