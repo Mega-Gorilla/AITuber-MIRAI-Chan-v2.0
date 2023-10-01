785c435523644e4df5702697490031e6a3087a21
Mega Gorilla <hahahadesu@gmail.com>
2023-10-01 14:10:36 +0900

    とりあえずGPTの応答を出力できる状態まで作成

--- API.py
+++ API.py
@@ -19,12 +19,13 @@ class GlobalValues:
     talkFlag = False
     speechToText = True
 
+def generate_table(history=5):
+    return
 #
 async def post_data(data_dict):
     async with httpx.AsyncClient() as client:
         response = await client.post(f"{BASE_URL}/custom/add", json=data_dict)
         if response.status_code == 200:
-            print("Data posted successfully!")
             return response.json()
         else:
             print(f"Failed to post data. Status code: {response.status_code}")
@@ -52,10 +53,11 @@ async def request_GPT(ID,prompt_name, user_prompt, variables,stream):
     }
     request_URL = (f"{BASE_URL}/requst/openai-post/{prompt_name}")
     
-    print(data)
-    async with httpx.AsyncClient() as client:
+    async with httpx.AsyncClient(timeout=60.0) as client:
         response = await client.post(request_URL, json=data, params=params)
-        return {"ID":ID,"message":response}
+        print_responce = response.text.replace('\n', '\n')
+        #print(f"Request_GPT < ID:{ID} > \nmessage:{print_responce}\njson:{data}\n")
+        return {"ID":ID,"message":response.text}
     
 async def get_queue_request(url):
     try:
@@ -78,18 +80,28 @@ async def get_queue_request(url):
 # 処理結果に基づいて次のタスクを決定する
 async def handle_results(result_queue,task_queue):
     chatLog=''
-    chatLog_lineMax=5
+    chatLog_lineMax=6
     summary=''
     mirai_example=''
+    summarize = False
 
     while True:
-        global talkFlag
 
         #結果を取得する
         result = await result_queue.get()
         result_ID = result["ID"]
         result_message = result["message"]
-        print(f"結果ID:{result_ID}/nMessage:{result_message}\n\n")
+        print_result_message = result_message.replace("\\n", "\n")
+        print(f"結果ID:{result_ID} - Message:{print_result_message}\n")
+        
+        #グローバル関数更新
+        global_dict = await get_data()
+        if "talkFlag" in global_dict:
+            GlobalValues.talkFlag = global_dict["talkFlag"]
+            if GlobalValues.talkFlag:
+                print("問い合わせON..")
+        if "speechToText" in global_dict:
+            GlobalValues.speechToText = global_dict["speechToText"]
         
         # User会話データがあった時、
         if result_ID == 'speech':
@@ -99,34 +111,46 @@ async def handle_results(result_queue,task_queue):
 
             #メモリより類似会話の検索
 
-            if talkFlag: #Talkボタンが押された時
-                talkFlag = False
+            if GlobalValues.talkFlag: #Talkボタンが押された時
+                GlobalValues.talkFlag = False
 
                 miraichan_talk_task = request_GPT(ID="talkStr",
-                                                  prompt_name='miraiV2.json',
+                                                  prompt_name='miraiV2',
                                                   user_prompt=[],
                                                   variables={
                                                       "chatLog":chatLog,
                                                         "summary":summary,
                                                         "mirai_example":mirai_example},
                                                     stream=True)
+                #グローバル配列情報の更新
+                global_dict = await get_data()
+                global_dict["talkFlag"] = False
+                await post_data(global_dict)
+                #Jobを追加
                 await task_queue.put(miraichan_talk_task)
 
         # Mirai Chan Talk が出力された時、
         if result_ID == 'talkStr':
+            index_of_colon = result_message.index(":")
+            # コロンの前の部分が10文字以下である場合、コロンを含めて削除
+            if index_of_colon <= 10:
+                result_message = result_message[index_of_colon+1:]
+            
             #みらい　会話履歴の追加
             chatLog+=(f"Mirai:{result_message}\n")
 
         # 要約が出力されたとき、
         if result_ID == 'summaryStr':
+            print(f"要約の実施\n{chatLog}->{result_message}")
             #要約変数に要約結果を追加
             summary = result_message
             #ショートメモリから要約した内容を消去
             chatlog_lines = chatLog.split("\n")
             chatLog = "\n".join(chatlog_lines[:-chatLog_lineMax])
+            summarize = False
 
         # ショートメモリ(chatlog)が一定行数以上になった時要約を依頼
-        if chatLog.count('\n')+1 > chatLog_lineMax:
+        if chatLog.count('\n')+1 > chatLog_lineMax and summarize == False:
             #ショートメモリが一定数以上になった時要約する
             summary_task = request_GPT(ID='summaryStr',
                                        prompt_name='talkSummary',
@@ -136,6 +160,7 @@ async def handle_results(result_queue,task_queue):
                                                 "summary":summary,
                                                 "chat_log":chatLog},
                                         stream=True)
+            summarize = True
             await task_queue.put(summary_task)
         
         await asyncio.sleep(0.1)
@@ -156,8 +181,8 @@ async def main():
     # Start a task to handle results
     asyncio.create_task(handle_results(result_queue,task_queue))
     
-    async def wrapper(func, *args):
-        result = await func(*args)
+    async def wrapper(task):
+        result = await task
         await result_queue.put(result)
 
     while True:
@@ -169,24 +194,19 @@ async def main():
             GlobalValues.speechToText = global_dict["speechToText"]
 
         #Azure Speechが終了or実行されていない時、Speechを起動する。
-        print(GlobalValues.speechToText)
         if  GlobalValues.speechToText and (speech_task is None or speech_task.done()):
-            print("OK")
             speech_task=asyncio.create_task(handler.from_mic())
         
         #新しいタスクを作成
         tasks_to_run = []
 
-        if task_queue.qsize()!=0:
-            tasks= await task_queue.get()
-        #タスクリストから、子タスクを分割し、タスクリストに変換する。
-        for task in tasks:
-            add_task=(task['func'],task['value'])
-            tasks_to_run.append(add_task)
-        
-        # 定義に基づいてタスクを動的に生成
-        tasks = [asyncio.create_task(wrapper(func, *args)) for func, args in tasks_to_run]
-        await asyncio.gather(*tasks)
+        while not task_queue.empty():
+            task = await task_queue.get()
+            tasks_to_run.append((task,))
+        if tasks_to_run:
+            # 定義に基づいてタスクを動的に生成
+            tasks = [asyncio.create_task(wrapper(task)) for task, in tasks_to_run]
+            #await asyncio.gather(*tasks)
             
         await asyncio.sleep(0.5)
 
