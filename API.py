5d931da2c432770d4d1480168ce689b7b670330a
Mega Gorilla <hahahadesu@gmail.com>
2023-10-01 17:05:57 +0900

    Ex.Memoryを実装　UIを変更

--- API.py
+++ API.py
@@ -1,6 +1,10 @@
 #API.py
 from module.azure_speech_handler import SpeechHandler
 from rich import print
+from rich.table import Table
+from rich.console import Console
+from collections import deque
+from find_similar import AnswerFinder
 import os
 import asyncio
 import httpx
@@ -9,8 +13,13 @@ import httpx
 speech_key = os.getenv("AZURE_API_KEY")
 speech_region = "japaneast"
 
+#Streamer memory
+directory = 'C:/Users/MegaGorilla/Documents/AI/AI_tuber_Train_Data/音声→文字データ/2.字幕_クリーニング済み'
+
 #URL
 BASE_URL = "http://127.0.0.1:8000"
+
+console = Console()
 
 class GlobalValues:
     UserName = "猩々博士"
@@ -19,8 +28,20 @@ class GlobalValues:
     talkFlag = False
     speechToText = True
 
-def generate_table(history=5):
-    return
+def display_table(data_history, columns):
+    """データの履歴とカラム名を受け取り、それをrichのテーブルとして表示する関数"""
+    table = Table(show_header=True, header_style="bold magenta",show_lines=True)
+    
+    # カラムをテーブルに動的に追加
+    for col_name in columns:
+        table.add_column(col_name)
+
+    # 履歴のデータをテーブルに追加
+    for row_data in data_history:
+        table.add_row(*map(str, row_data))
+
+    console.clear()
+    console.print(table)
 #
 async def post_data(data_dict):
     async with httpx.AsyncClient() as client:
@@ -80,26 +101,31 @@ async def get_queue_request(url):
 # 処理結果に基づいて次のタスクを決定する
 async def handle_results(result_queue,task_queue):
     chatLog=''
-    chatLog_lineMax=6
+    chatLog_lineMax=3
     summary=''
     mirai_example=''
     summarize = False
 
+    table_history = deque(maxlen=3)
+    column_names = ["Job Status", "Short Memory", "Long Memory", "Ex. Memory"]
+
+    #Streamer Example
+    print("ベクトルを作成中....")
+    finder = AnswerFinder(directory)
+    print("ベクトル作成完了")
+
     while True:
+        job_status=''
 
         #結果を取得する
         result = await result_queue.get()
         result_ID = result["ID"]
         result_message = result["message"]
-        print_result_message = result_message.replace("\\n", "\n")
-        print(f"結果ID:{result_ID} - Message:{print_result_message}\n")
         
         #グローバル関数更新
         global_dict = await get_data()
         if "talkFlag" in global_dict:
             GlobalValues.talkFlag = global_dict["talkFlag"]
-            if GlobalValues.talkFlag:
-                print("問い合わせON..")
         if "speechToText" in global_dict:
             GlobalValues.speechToText = global_dict["speechToText"]
         
@@ -109,10 +135,14 @@ async def handle_results(result_queue,task_queue):
             userTalkStr = result_message
             chatLog += (f"{GlobalValues.UserName}:{userTalkStr}\n")
 
-            #メモリより類似会話の検索
+            job_status+='ユーザー会話追加\n'
 
             if GlobalValues.talkFlag: #Talkボタンが押された時
                 GlobalValues.talkFlag = False
+
+                #メモリより類似会話の検索
+                streamer_ex = finder.find_similar(userTalkStr)
+                mirai_example = '\n'.join([item[0] for item in streamer_ex])
 
                 miraichan_talk_task = request_GPT(ID="talkStr",
                                                   prompt_name='miraiV2',
@@ -126,28 +156,37 @@ async def handle_results(result_queue,task_queue):
                 global_dict = await get_data()
                 global_dict["talkFlag"] = False
                 await post_data(global_dict)
+
+                job_status+='みらい会話問い合わせ実施\n'
                 #Jobを追加
                 await task_queue.put(miraichan_talk_task)
 
         # Mirai Chan Talk が出力された時、
         if result_ID == 'talkStr':
-            index_of_colon = result_message.index(":")
-            # コロンの前の部分が10文字以下である場合、コロンを含めて削除
-            if index_of_colon <= 10:
-                result_message = result_message[index_of_colon+1:]
-            
+            if ":" in result_message:  # コロンが存在するか確認
+                index_of_colon = result_message.index(":")
+                # コロンの前の部分が20文字以下である場合、コロンを含めて削除
+                if index_of_colon <= 20:
+                    result_message = result_message[index_of_colon+1:]
+
+            result_message = result_message.replace("\n", "")
+            result_message = result_message.replace('"', "")
             #みらい　会話履歴の追加
-            chatLog+=(f"Mirai:{result_message}\n")
+            chatLog+=(f"Mirai Chan:{result_message}")
+
+            job_status+='みらい会話発話実施\n'
 
         # 要約が出力されたとき、
         if result_ID == 'summaryStr':
-            print(f"要約の実施\n{chatLog}->{result_message}")
+            #print(f"要約の実施\n{chatLog}->{result_message}")
             #要約変数に要約結果を追加
             summary = result_message
             #ショートメモリから要約した内容を消去
             chatlog_lines = chatLog.split("\n")
-            chatLog = "\n".join(chatlog_lines[:-chatLog_lineMax])
+            chatLog = "\n".join(chatlog_lines[chatLog_lineMax:])
             summarize = False
+
+            job_status+='要約適応実施'
 
         # ショートメモリ(chatlog)が一定行数以上になった時要約を依頼
         if chatLog.count('\n')+1 > chatLog_lineMax and summarize == False:
@@ -161,9 +200,15 @@ async def handle_results(result_queue,task_queue):
                                                 "chat_log":chatLog},
                                         stream=True)
             summarize = True
+            job_status+= '要約作成実施'
             await task_queue.put(summary_task)
         
-        await asyncio.sleep(0.1)
+        #UIを更新
+        table_history.append([job_status,chatLog,summary,mirai_example])  # 最新のデータを履歴に追加
+        display_table(table_history, column_names)  # テーブルを表示
+        mirai_example=""
+
+        await asyncio.sleep(0.5)
 
 async def main():
     task_queue = asyncio.Queue()
@@ -208,7 +253,7 @@ async def main():
             tasks = [asyncio.create_task(wrapper(task)) for task, in tasks_to_run]
             #await asyncio.gather(*tasks)
             
-        await asyncio.sleep(0.5)
+        await asyncio.sleep(0.1)
 
 if __name__ == "__main__":
     asyncio.run(main())
