2c994bb7aff505755c685a31ba73dd0cbabb1d5a
Mega-Gorilla <hahahadesu@gmail.com>
2023-11-11 01:15:55 +0900

    pychatからyoutubeAPIに取得方式を変更

--- module/live_chat_fetcher.py
+++ module/live_chat_fetcher.py
@@ -1,12 +1,17 @@
-from module.server_requests import *
 import re
-import pytchat
+import os
 import asyncio
 import requests
+
 try:
     from module.rich_desgin import error
+    from module.server_requests import *
 except ImportError:
     from rich_desgin import error
+    from server_requests import *
+
+class config:
+    pychat_model = None
 
 def extract_video_id(url):
     """
@@ -35,7 +40,7 @@ def get_channel_id(URL,API_key):
     channel_id = data['items'][0]['snippet']['channelId']
     return channel_id
 
-def get_new_comments(chat):
+def get_new_comments(video_id,api_key):
     """
     YouTubeのライブチャットから新しいコメントを取得します。
 
@@ -60,77 +65,39 @@ def get_new_comments(chat):
             - 'superchat_value' (float): スーパーチャットの金額。
             - 'superchat_currency' (str): スーパーチャットの通貨。
 
-    参考:
-        pytchatのデフォルトプロセッサに関する詳細は以下のURLを参照してください。
-        https://github.com/taizan-hokuto/pytchat/wiki/DefaultProcessor_
     """
-    new_comments = []
-    while chat.is_alive():
-        for c in chat.get().sync_items():
-            if c.amountValue>0:
-                superchat_bool=True
+    comments_list = []
+    # YouTube APIのURL
+    live_chat_url = f"https://www.googleapis.com/youtube/v3/videos?id={video_id}&part=liveStreamingDetails&key={api_key}"
+    # ライブチャットIDを取得する
+    response = requests.get(live_chat_url)
+    if response.status_code == 200:
+        live_chat_data = response.json()
+        live_chat_id = live_chat_data.get('items', [])[0]['liveStreamingDetails'].get('activeLiveChatId')
+        if live_chat_id:
+            # ライブチャットのコメントを取得する
+            live_chat_comments_url = f"https://www.googleapis.com/youtube/v3/liveChat/messages?liveChatId={live_chat_id}&part=id,snippet,authorDetails&key={api_key}"
+            comments_response = requests.get(live_chat_comments_url)
+            if comments_response.status_code == 200:
+                comments_data = comments_response.json()
+                for item in comments_data.get('items', []):
+                    # コメントの情報を辞書として抽出
+                    comment_info = {
+                        'name': item['authorDetails']['displayName'],
+                        'comment': item['snippet']['displayMessage'],
+                        'timestamp': item['snippet']['publishedAt'],
+                        'superchat_bool': 'superChatDetails' in item['snippet'],
+                        'superchat_value': float(item['snippet'].get('superChatDetails', {}).get('amountMicros', 0)) / 1e6 if 'superChatDetails' in item['snippet'] else 0,
+                        'superchat_currency': item['snippet'].get('superChatDetails', {}).get('currency', '')
+                    }
+                    comments_list.append(comment_info)
             else:
-                superchat_bool=False
-            new_comments.append({'name': c.author.name, 'comment': c.message,'timestamp':c.timestamp,'superchat_bool':superchat_bool,'superchat_value':c.amountValue,'superchat_currency':c.currency})
-        break  # ループを一度だけ実行
-    return new_comments
-
-def create_pychat(URL):# ビデオIDを抽出
-    video_id = extract_video_id(URL)
-    if not video_id:
-        error("pychat error","Invalid YouTube URL",{"Youtube URL":URL})
-        return None
-    chat = pytchat.create(video_id=video_id)
-    return chat
-
-async def youtube_liveChat_fetch(URL=None,chat=None):
-    """
-    YouTubeライブチャットからリアルタイムでコメントを取得します。
-
-    指定されたURLからライブチャットを作成し、そのチャットオブジェクトを通じて
-    新しいコメントのリストを取得します。コメントは辞書のリストとして返され、
-    各辞書にはコメント投稿者の名前、コメント内容、タイムスタンプ、スーパーチャットに関する情報が含まれます。
-
-    もし `chat` パラメータが None であれば、`URL` を使って新しいチャットオブジェクトを作成します。
-    この関数は非同期で実行されるため、`await` を使用して呼び出す必要があります。
-
-    戻り値の例:
-    [
-        {'name': '高山はるかしゅ', 'comment': '笹', 'timestamp': 1698983722670,
-         'superchat_bool': False, 'superchat_value': 0.0, 'superchat_currency': ''},
-        ...
-    ]
-
-    パラメータ:
-        URL (str): YouTubeライブチャットのURL。chatがNoneの場合に使用されます。
-        chat (Chat): pytchatを使用して生成されたチャットオブジェクト。
-                     このパラメータが提供されている場合、URLは無視されます。
-
-    戻り値:
-        list of dict: チャットの各コメント情報を含む辞書のリスト。
-            各辞書は以下のキーを持ちます:
-            - 'name' (str): コメント投稿者の名前。
-            - 'comment' (str): コメントの内容。
-            - 'timestamp' (int): コメントが投稿された時のUNIXタイムスタンプ。
-            - 'superchat_bool' (bool): コメントがスーパーチャットであるかどうか。
-            - 'superchat_value' (float): スーパーチャットの金額。
-            - 'superchat_currency' (str): スーパーチャットで使用された通貨。
-
-    例外:
-        この関数は、チャットの取得中に発生する可能性のある任意の例外をキャッチし、
-        エラーログに記録した後、処理を停止します。
-    """
-    if chat is None:
-        chat = create_pychat(url)
-    try:
-        # 新しいコメントを取得
-        new_comments = get_new_comments(chat)
-        
-        return new_comments
-
-    except Exception as e:
-        error("Live Chat Fetch Error:", f"{e}", {"URL":URL,"Chat":chat})
-        print("Stopped.")
+                {"ok":False,"message":f"Failed to get live chat messages: {comments_response.status_code}"}
+        else:
+            {"ok":False,"message":"No active live chat id found."}
+    else:
+        return{"ok":False,"message":f"Failed to get live chat ID: {response.status_code}"}
+    return comments_list
 
 async def youtube_viewer_count(URL,API_key):
     video_id = extract_video_id(URL)
@@ -140,7 +107,7 @@ async def youtube_viewer_count(URL,API_key):
     # レスポンスから視聴者数を抽出
     if 'liveStreamingDetails' in responce['items'][0]:
         viewer_count = responce['items'][0]['liveStreamingDetails'].get('concurrentViewers')
-        return {'ok':True,'viewer_count':viewer_count}
+        return {'ok':True,'viewer_count':int(viewer_count)}
     else:
         return{'ok':False,'message':'Live streaming details could not be found.'}
 
@@ -148,21 +115,17 @@ async def youtube_subscriber_count(channel_ID,API_key):
     api_url = f'https://www.googleapis.com/youtube/v3/channels?part=statistics&id={channel_ID}&key={API_key}'
     responce = await get_data_from_server(api_url)
     subscriber_count = responce['items'][0]['statistics']['subscriberCount']
-    return {'ok':True,'subscriber_count':subscriber_count}
+    return {'ok':True,'subscriber_count':int(subscriber_count)}
 
-async def loop_test(chat,interval_s=5):
+if __name__ == "__main__":
+    url='https://www.youtube.com/watch?v=tfyTjeEgmDA'
     while True:
-        new_comments = await youtube_liveChat_fetch(chat=chat)
+        
+        new_comments = get_new_comments(extract_video_id(url),os.getenv("GOOGLE_API_KEY"))
         print(new_comments)
         for comment in new_comments:
             if comment['superchat_bool']:
                 print(f"SUPER CHAT!!! {comment['superchat_currency']} {comment['superchat_value']}/{comment['name']}: {comment['comment']}")
             else:
                 print(f"{comment['name']}: {comment['comment']}")
-        await asyncio.sleep(interval_s)
-
-
-if __name__ == "__main__":
-    url='https://www.youtube.com/watch?v=_p2zEtZR5OQ'
-    chat=create_pychat(url)
-    asyncio.run(loop_test(chat))
+        asyncio.sleep(5)
