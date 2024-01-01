b6c2f8f74c1bd22fd365f445e431dc956c38a54d
Mega-Gorilla <hahahadesu@gmail.com>
2024-01-01 18:24:59 +0900

    GPT4プロンプト省略版作成

--- module/LLM_Process.py
+++ module/LLM_Process.py
@@ -81,7 +81,7 @@ async def process_airi_v17(request_id):
                     for expression_name in expression_list:
                         if expression_name in expression_text:
                             #表情の更新
-                            console.print("表情変更: ",style='blue',end='')
+                            console.print("<Unity> 表情変更: ",style='blue',end='')
                             print(expression_name)
                             requests.post(url=f"{config.AI_Tuber_URL}/Unity/animation/post/",json={"VRM_expression": expression_name})
 
@@ -107,7 +107,7 @@ async def process_airi_v17(request_id):
 
                 if start_marker == "statements:":
                     #読み上げ実施
-                    console.print("読み上げ実施:",style='blue',end='')
+                    console.print("<Voice> 読み上げ実施:",style='blue',end='')
                     print(item)
                     stream = audio_stream_start()
                     audio_data = text_to_wave(item,preset_id=1,speaker=voicevox.style_id)
@@ -146,14 +146,22 @@ async def process_airi_v17(request_id):
             elif 'gemini' in model_name:
                 if request[0]['completion_tokens'] < LLM.completion_token_summary_trigger[model_name]:
                     break
+
+            process_dict = requests.get(f"{config.AI_Tuber_URL}/summary_process/get/").json()
+            if process_dict['summary_talk'] != True and process_dict['summary_game'] != True :
+                #要約中の場合
+                console.print("--- <要約中のため要約をパスしました> ----",style='yellow')
+                break
+
             #要約フラグ作成
-            print("ログを要約します")
+            console.print("--- <ログを要約します> ----",style='yellow')
+            requests.post(f"{config.AI_Tuber_URL}/summary_process/post/?summary_process=true") # 要約process True
             requests.post(f"{config.AI_Tuber_URL}/LLM/request/post/?prompt_name=talk_logTosummary&stream=false")
             #ゲームプレイ中は、ゲームログ要約も実施
-            if requests.get(f"{config.AI_Tuber_URL}/GameName/get")!= "":
+            if requests.get(f"{config.AI_Tuber_URL}/GameName/get").json()!= "":
+                requests.post(f"{config.AI_Tuber_URL}/summary_process/post/?summray_game_process=true")
                 requests.post(f"{config.AI_Tuber_URL}/LLM/request/post/?prompt_name=game_logTosummary&stream=false")
             break
-
     await asyncio.sleep(1)
 
 async def Statement_to_animMotion(request_id):
@@ -161,7 +169,7 @@ async def Statement_to_animMotion(request_id):
     gesture_str = request[0]['content']
     if gesture_str != "None":
         requests.post(url=f"{config.AI_Tuber_URL}/Unity/animation/post/",json={"VRM_animation": gesture_str})
-        console.print("モーションを変更",style='green',end='')
+        console.print("<Unity> モーションを変更:",style='green',end='')
         print(gesture_str)
 
 async def process_talk_logTosummary(request_id):
@@ -170,6 +178,8 @@ async def process_talk_logTosummary(request_id):
     request = requests.get(f"{config.GPT_Mangaer_URL}/LLM/get/?reset_all=false&del_request_id={request_id}").json()
     summary = request[0]['content']
     requests.post(f"{config.GPT_Mangaer_URL}/summary/post?summary={summary}")
+    requests.post(f"{config.AI_Tuber_URL}/summary_process/post/?summary_process=false")
+    
     await asyncio.sleep(1)
 
 async def process_game_logTosummary(request_id):
@@ -178,4 +188,5 @@ async def process_game_logTosummary(request_id):
     request = requests.get(f"{config.GPT_Mangaer_URL}/LLM/get/?reset_all=false&del_request_id={request_id}").json()
     summary = request[0]['content']
     requests.post(f"{config.GPT_Mangaer_URL}/GameData/summary/post?summary={summary}")
+    requests.post(f"{config.AI_Tuber_URL}/summary_process/post/?summray_game_process=false")
     await asyncio.sleep(1)
