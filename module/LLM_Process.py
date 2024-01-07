19649e76095e83fdaf47311670aa64d3a75218d0
Mega-Gorilla <hahahadesu@gmail.com>
2024-01-08 01:21:16 +0900

    GPTManagerの変更に対応、VoiceVox読み上げを非同期ように修正

--- module/LLM_Process.py
+++ module/LLM_Process.py
@@ -2,7 +2,7 @@ import requests,asyncio
 from module.voicevox import *
 from module.LLM_Request import summary_data
 from rich.console import Console
-from module.rich_desgin import error
+from module.rich_desgin import error,warning_message
 import time
 
 """
@@ -10,7 +10,7 @@ import time
 どのプロンプトに対してどの関数が対応しているかは、main.py LLM_config.function_mapを参照のこと
 """
 console = Console()
-
+ 
 class config:
     #URL
     GPT_Mangaer_URL = "http://127.0.0.1:8000"
@@ -23,20 +23,20 @@ class LLM:
     total_token_summary_trigger = {"gpt-4-1106-preview":3171,"gpt-4":6143} #"gpt-4-1106-preview":3171,"gpt-4":6143
     completion_token_summary_trigger = {"gemini-pro":6144}
 
-    loop_break_time = 180 #s
+    loop_break_time = 60 #s
 
-class voicevox:
+class voicevox_config:
     # 一時的、VoiceVox移行後消去
     voicevox_name = 'AIRI'
     speaker_uuid = "9f3ee141-26ad-437e-97bd-d22298d02ad2"
     style_id = 20
-    parameters = [{'name':'neutral','Speaking_Speed':1.2,'Voice_Pitch':0,'Voice_Intonation':1},
-                 {'name':'anger','Speaking_Speed':1.2,'Voice_Pitch':-0.05,'Voice_Intonation':0.8},
-                 {'name':'disgust','Speaking_Speed':1.2,'Voice_Pitch':-0.1,'Voice_Intonation':0.6},
-                 {'name':'fear','Speaking_Speed':1.4,'Voice_Pitch':0.08,'Voice_Intonation':1.2},
-                 {'name':'happy','Speaking_Speed':1.2,'Voice_Pitch':0.05,'Voice_Intonation':1.5},
-                 {'name':'sad','Speaking_Speed':0.9,'Voice_Pitch':0.09,'Voice_Intonation':1.3},
-                 {'name':'surprise','Speaking_Speed':1.2,'Voice_Pitch':0.05,'Voice_Intonation':1.1}]
+    parameters = [{'name':'neutral','Speaking_Speed':1.2,'Voice_Pitch':0,'Voice_Intonation':1,'volumeScale':1},
+                 {'name':'anger','Speaking_Speed':1.2,'Voice_Pitch':-0.05,'Voice_Intonation':0.8,'volumeScale':1},
+                 {'name':'disgust','Speaking_Speed':1.2,'Voice_Pitch':-0.1,'Voice_Intonation':0.6,'volumeScale':1},
+                 {'name':'fear','Speaking_Speed':1.4,'Voice_Pitch':0.08,'Voice_Intonation':1.2,'volumeScale':1},
+                 {'name':'happy','Speaking_Speed':1.2,'Voice_Pitch':0.05,'Voice_Intonation':1.5,'volumeScale':1},
+                 {'name':'sad','Speaking_Speed':0.9,'Voice_Pitch':0.09,'Voice_Intonation':1.3,'volumeScale':1},
+                 {'name':'surprise','Speaking_Speed':1.2,'Voice_Pitch':0.05,'Voice_Intonation':1.1,'volumeScale':1}]
 
 async def process_airi_v17(request_id):
     """
@@ -47,16 +47,24 @@ async def process_airi_v17(request_id):
     content = ""
     done = False
     #切り取りマーカーリスト
-    markers = [["# Organize Your Thoughts:","#Result:"],["# Organize Your Thoughts:","# Result:"],["emotion:","facial expression:"],["facial expression:","statements:"],["statements:","gesture:"],["gesture:","emotion:"],["gesture:","--<Done>--"]]
+    markers = [["# Organize Your  Thoughts:","#Result:"],["# Organize Your Thoughts:","# Result:"],["emotion:","facial expression:"],["facial expression:","statements:"],["statements:","gesture:"],["gesture:","emotion:"],["gesture:","--<Done>--"]]
     content_list = []
+    request = None
     
     while True:
+        voice_params = voicevox_config.parameters[0]
         request = requests.get(f"{config.GPT_Mangaer_URL}/LLM/get-chunk/?reset_all=false&del_request_id={request_id}").json()
-        #request = await get_data_from_server(URL=f"{config.GPT_Mangaer_URL}/LLM/get-chunk/?reset_all=false&del_request_id={request_id}")
+        if request == []:
+            await asyncio.sleep(0.2)
+            continue
+
         for item in request:
             content += item['content']
-            if item['finish_reason']=="Done":
+            console.print(item['content'],style='blue',end='')
+            if item['finish']==True:
                 content += "\n--<Done>--"
+                if item["finish_reason"] != "stop":
+                    warning_message(f"GPT Request Finish Reason is Not [Stop].\nfinish_reason:{item['finish_reason']}")
                 done = True
         
         # レスポンス結果より必要なデータを抜き出し
@@ -76,10 +84,10 @@ async def process_airi_v17(request_id):
                         if emotion_name in emotion_text:
 
                             if config.talker == 'voicevox':
-                                for param in voicevox.parameters:
+                                for param in voicevox_config.parameters:
                                     if param['name'] == emotion_name:
                                         #VoiceVoxプリセット切り替え
-                                        update_preset(1,voicevox.voicevox_name,voicevox.speaker_uuid,voicevox.style_id,param['Speaking_Speed'],param['Voice_Pitch'],param['Voice_Intonation'])
+                                        voice_params = param
                     await asyncio.sleep(0)
                 
                 if start_marker == "facial expression:":
@@ -88,7 +96,7 @@ async def process_airi_v17(request_id):
                     for expression_name in expression_list:
                         if expression_name in expression_text:
                             #表情の更新
-                            console.print("<Unity> 表情変更: ",style='blue',end='')
+                            console.print("<Unity> 表情変更: ",style='green',end='')
                             print(expression_name)
                             requests.post(url=f"{config.AI_Tuber_URL}/Unity/animation/post/",json={"VRM_expression": expression_name})
                     await asyncio.sleep(0)
@@ -113,18 +121,27 @@ async def process_airi_v17(request_id):
                         requests.post(url=f"{config.AI_Tuber_URL}/LLM/request/post/?prompt_name=Statement_to_animMotion_2&stream=false",json=request_data)
                     await asyncio.sleep(0)
                             
-
                 if start_marker == "statements:":
-                    #読み上げ実施
-                    console.print("<Voice> 読み上げ実施:",style='blue',end='')
-                    print(item)
-                    stream = audio_stream_start()
-                    audio_data = text_to_wave(item,preset_id=1,speaker=voicevox.style_id)
-                    stream.write(audio_data)
-                    audio_stream_stop(stream)
+
+                    voicevox_post_data = {
+                        "chara_name": "アイリ",
+                        "text": item,
+                        "subs": True,
+                        "lipsync": True,
+                        "speaker_uuid": voicevox_config.speaker_uuid,
+                        "style_id": voicevox_config.style_id,
+                        "speedScale": voice_params['Speaking_Speed'],
+                        "pitchScale": voice_params['Voice_Pitch'],
+                        "intonationScale": voice_params['Voice_Intonation'],
+                        "volumeScale": voice_params['volumeScale'],
+                        "prePhonemeLength": 0.1,
+                        "postPhonemeLength": 0.1
+                        }
+                    #読み上げリクエスト
+                    requests.post(f"{config.AI_Tuber_URL}/text_to_vice/voicevox/post",json=voicevox_post_data)
 
                     #会話ログへの追加
-                    requests.post(url=f"{config.AI_Tuber_URL}/talk_log/post",json={"アイリ":item})
+                    requests.post(url=f"{config.AI_Tuber_URL}/talk_log/post",json=[{"アイリ":item}])
                     await asyncio.sleep(0)
 
                 content_list.append({start_marker:item})
@@ -132,17 +149,21 @@ async def process_airi_v17(request_id):
                 content = content[end_index:]
                 console.print(f"{start_marker.upper()}",style="green",end='')
                 print(item)
+                await asyncio.sleep(0.2)
         
         if done:
             break
         if time.time() - process_airi_v17_start_time > LLM.loop_break_time:
-            error('Task Timeout.','3分経過したため、タスクは強制終了されました。',{'GPT_Raw_data':request,'Result':content_list})
+            print(request)
+            error('Task Timeout.',f'{LLM.loop_break_time}秒経過したため、タスクは強制終了されました。',{'GPT_Raw_data':request,'Result':content_list})
             break
         await asyncio.sleep(0.2)
+    console.print("\n------------------アイリ v17 Result------------------",style='blue')
     for dict_data in content_list:
         for key,value in dict_data.items():
             console.print(f"{key}",style='green',end='')
             print(value)
+    console.print("------------------END------------------\n",style='blue')
 
     process_airi_v17_start_time = time.time()
     #Check Tokens
@@ -150,7 +171,8 @@ async def process_airi_v17(request_id):
         #Stream後のLLM結果を受信
         request = requests.get(f"{config.GPT_Mangaer_URL}/LLM/get/?reset=false&del_request_id={request_id}").json()
         if time.time() - process_airi_v17_start_time > LLM.loop_break_time:
-            error('Task Timeout.','3分経過したため、タスクは強制終了されました。 Stream後の結果を受信できませんでした。',{'GPT_Raw_data':request,'Result':content_list})
+            print(request)
+            error('Task Timeout.',f'{LLM.loop_break_time}秒経過したため、タスクは強制終了されました。 Stream後の結果を受信できませんでした。',{'GPT_Raw_data':request,'Result':content_list})
             break
         if len(request)==0:
             await asyncio.sleep(1)
@@ -183,7 +205,11 @@ async def process_airi_v17(request_id):
 
 async def Statement_to_animMotion(request_id):
     request = requests.get(f"{config.GPT_Mangaer_URL}/LLM/get/?reset_all=false&del_request_id={request_id}").json()
-    gesture_str = request[0]['content']
+    try:
+        gesture_str = request[0]['content']
+    except Exception as e:
+        gesture_str = "None"
+        error("Error Statement_to_animMotion",e,{'request':request,'request_id':request_id})
     if gesture_str != "None":
         requests.post(url=f"{config.AI_Tuber_URL}/Unity/animation/post/",json={"VRM_animation": gesture_str})
         console.print("<Unity> モーションを変更:",style='green',end='')
@@ -192,7 +218,11 @@ async def Statement_to_animMotion(request_id):
 
 async def process_talk_logTosummary(request_id):
     #要約が受信された場合
-    request = requests.get(f"{config.GPT_Mangaer_URL}/LLM/get/?reset_all=false&del_request_id={request_id}").json()
+    for i in range(3):
+        request = requests.get(f"{config.GPT_Mangaer_URL}/LLM/get/?reset_all=false&del_request_id={request_id}").json()
+        if request != []:
+            break
+        await asyncio.sleep(1)
     summary = request[0]['content']
     console.print("--- <会話ログを要約しました> ----",style='yellow')
     print(summary)
@@ -207,7 +237,11 @@ async def process_talk_logTosummary(request_id):
 
 async def process_game_logTosummary(request_id):
     #ゲームログの要約が受信した場合
-    request = requests.get(f"{config.GPT_Mangaer_URL}/LLM/get/?reset_all=false&del_request_id={request_id}").json()
+    for i in range(3):
+        request = requests.get(f"{config.GPT_Mangaer_URL}/LLM/get/?reset_all=false&del_request_id={request_id}").json()
+        if request != []:
+            break
+        await asyncio.sleep(1)
     summary = request[0]['content']
     console.print("--- <ゲーム要約を要約しました> ----",style='yellow')
     print(summary)
@@ -215,7 +249,7 @@ async def process_game_logTosummary(request_id):
     requests.post(f"{config.AI_Tuber_URL}/summary_process/post/?summray_game_process=false")
     #要約したデータの消去
     talk_log = requests.get(f"{config.AI_Tuber_URL}/GameData/talk_log/get?reset=true").json()
-    old_talk_log = summary_data.chara_talk_log
+    old_talk_log = summary_data.game_talk_log
     filtered_talk_logs = [item for item in talk_log if item not in old_talk_log]
     requests.post(url=f"{config.AI_Tuber_URL}/GameData/talk_log/post",json=filtered_talk_logs)
     await asyncio.sleep(1)
